b18d3b38f538ed966ac27a814f7fe9c92c540a95
Prasanta Banerjee <83323601+spyder-ai@users.noreply.github.com>
2025-09-01 18:05:54 +0530

    Add files via upload

---
+++ Project_ 3/index.js
@@ -0,0 +1,15 @@
+function rolldice(){
+    const numofdice = document.getElementById("numofdice").value;
+    const diceresult = document.getElementById("diceresult");
+    const diceimages = document.getElementById("diceimages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numofdice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="dice_images/${value}.png" alt="dice ${value}">`);
+    }    
+    diceresult.textContent = `dice: ${values.join(`, `)}`;
+    diceimages.innerHTML = images.join(``);
+}
